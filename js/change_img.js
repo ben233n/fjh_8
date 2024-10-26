b1aa65b5da246948d96f26062d260208f45c5992
ben233n <154246877+ben233n@users.noreply.github.com>
2024-10-26 14:56:22 +0800

    v0.8

---
+++ js/change_img.js
@@ -0,0 +1,16 @@
+let article_2_img_1=document.getElementById('article_2_img_1');
+let article_2_img_2=document.getElementById('article_2_img_2');
+let article_2_img_3=document.getElementById('article_2_img_3');
+
+
+const bang_img_1=["./img/阿尼亞.png","./img/申有娜點開.jpg"];
+let timer=0;
+
+
+// setInterval(function(){
+//     timer = (timer+1)%bang_img_1.length;
+//     article_2_img_3.src=bang_img_1[timer];
+// },1000);
+
+
+
